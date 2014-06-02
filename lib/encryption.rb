7b182d7d93a8c7c144803c6e2a44cd61a8e5cacf
itgsod-pontus-egebergjacob <pontus.egebergjacobson@elev.it-gymnasiet.se>
2014-06-02 14:25:19 +0200

    added the alphabet

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -2,5 +2,8 @@ def encrypt(text, offset)
   raise ArgumentError, 'String must not be empty' if text == ''
   raise ArgumentError, 'Offset must not be zero' if offset == 0
 
+  alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
   return 'output'
 end
