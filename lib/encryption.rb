1a3b194f01da05da7c84356dc68a0dddd98fe928
itgsod-pontus-egebergjacob <pontus.egebergjacobson@elev.it-gymnasiet.se>
2014-06-02 14:14:56 +0200

    made it return a string(easy way)

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,5 +1,6 @@
 def encrypt(text, offset)
   raise ArgumentError, 'String must not be empty' if text == ''
   raise ArgumentError, 'Offset must not be zero' if offset == 0
-  
+
+  return 'output'
 end
