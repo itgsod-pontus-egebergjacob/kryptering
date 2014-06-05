74066a3c06002ed613b7696d11cd2e63b72d383c
itgsod-pontus-egebergjacob <pontus.egebergjacobson@elev.it-gymnasiet.se>
2014-06-05 11:10:23 +0200

    made the code decrypt as long as its less than 27

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,9 +1,29 @@
-def encrypt(text, offset)
-  raise ArgumentError, 'String must not be empty' if text == ''
+def encrypt(input_text, offset)
+  raise ArgumentError, 'String must not be empty' if input_text == ''
   raise ArgumentError, 'Offset must not be zero' if offset == 0
 
-  alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '']
+
+  input_text = input_text.upcase
+
+  output = ''
+
+  input_text.each_char do |char|
+
+    i = alphabet.index(char)
+
+    new_i = i
+
+    new_i += offset
+
+    value = alphabet[new_i]
+
+    output += value
+
+  end
 
 
-  return 'output'
+  return output
 end
+
+p encrypt('t', -3)
